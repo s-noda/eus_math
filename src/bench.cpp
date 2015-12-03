a0a8a892f8a8a94588a053cdc7161cee0bdaa7b9
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-03 17:47:59 +0900

    update a little

--- src/bench.cpp
+++ src/bench.cpp
@@ -2,28 +2,34 @@
 #include <sys/time.h>
 
 int main() {
-  int cnt = 1000000;
+  int cnt = 100000;
   double val = 1.0;
   double rate = 5.3;
+  int i;
 
   struct timeval s, t;
 
   std::cout << "multiplication " << cnt << "times" << std::endl;
+  val = 1.0;
+  i = 0;
   gettimeofday(&s, NULL);
-  for ( int i=0 ; i<cnt ; i++ ){
+  // for ( int i=0 ; i<cnt ; i++ ){
+  while ( i++ < cnt ){
     val = (val * rate);
   }
-  std::cout << 1 << "-->" << val << std::endl;
   gettimeofday(&t, NULL);
-  std::cout <<  ((t.tv_sec - s.tv_sec) * 1000 + (t.tv_usec - s.tv_usec) / 1000) << "ms" << std::endl;
+  std::cout << 1 << "-->" << val << std::endl;
+  std::cout <<  ((t.tv_sec - s.tv_sec) * 1000.0 + (t.tv_usec - s.tv_usec) / 1000.0) << "ms" << std::endl;
 
   std::cout << "addition " << cnt << "times" << std::endl;
   val = 1.0;
+  i = 0;
   gettimeofday(&s, NULL);
-  for ( int i=0 ; i<cnt ; i++ ){
+  while ( i++ < cnt ){
+  // for ( int i=0 ; i<cnt ; i++ ){
     val = (val + rate);
   }
-  std::cout << 1 << "-->" << val << std::endl;
   gettimeofday(&t, NULL);
-  std::cout <<  ((t.tv_sec - s.tv_sec) * 1000 + (t.tv_usec - s.tv_usec) / 1000) << "ms" << std::endl;
+  std::cout << 1 << "-->" << val << std::endl;
+  std::cout <<  ((t.tv_sec - s.tv_sec) * 1000.0 + (t.tv_usec - s.tv_usec) / 1000.0) << "ms" << std::endl;
 }
